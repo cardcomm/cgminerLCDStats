9d90f2942fd34e3c47943e68d39b1cd85a593911
cardcomm <rke@bizdistrict.com>
2013-08-01 02:24:35 -0500

    temp fix for buffer size

--- CgminerRPCClient.py
+++ CgminerRPCClient.py
@@ -17,7 +17,7 @@ def command(command, host, port):
     s.connect((host, port))
     s.sendall(json.dumps({"command": command})) # send API request formatted as json
     time.sleep(0.03)
-    data = s.recv(8192)
+    data = s.recv(65433)
     s.close()
     
   except Exception as e:
