df758e6e0811e027099cef894a8ad141fb224060
cardcomm <rke@bizdistrict.com>
2013-08-11 18:30:08 -0500

    Update cgminerLCDStats.py

--- cgminerLCDStats.py
+++ cgminerLCDStats.py
@@ -1,19 +1,24 @@
 #
 # Simple script to read information from the cgminer API and display it on
-#  the AIDA64 LCD Smartie display
+#  the "LCD sys info" display
 #
 # Copyright 2013 Cardinal Communications
 # If you feel this code is useful, please consider a donation to:
 #  BTC address: 15aGZp2pCbpAFHcjHVrx2G746PXFo9VEed
 #
 # Note: A HUGE thank you goes out to Kano for is invaluable assitance with this code
-#       He's a key developer on the cgminer project, and was a big help.
+#       He's a key developer on the cgminer project, and was a big help with this small project as well.
 #       https://bitcointalk.org/index.php?action=profile;u=36044
 #
-# For more specifics about the display this code supports, see:
+# For more specifics about the "LCD sys info" display this code supports, see:
 #  http://coldtearselectronics.wikispaces.com/USB+LCD+-+LCD+System+info
 #  https://github.com/dangardner/pylcdsysinfo
+#
+#  To purchase one see here:
 #  http://www.ebay.com/itm/USB-2-8-TFT-LCD-module-LCD-sys-info-display-temperature-fan-AIDA64-LCD-Smartie-/121004607232?pt=LH_DefaultDomain_0&hash=item1c2c6fc700
+#
+# NOTE: I am NOT associated in any way with ColdTears Electronics. All I can promise is that it works well for what I use it for.
+
 
 import math
 import sys
